ceb65a75fd9e6d9771a2a8e761381f7d3e714b18
Edmund Oh <eddieoh0@gmail.com>
2022-05-11 15:35:20 +0900

    updated

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,47 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new # needed to instantiate the form_for
+  end
+
+  def create
+    @restaurants = Restaurant.new(params[restaurant_params])
+    if @restaurant.save # if it does save, go to the show page to see it's been saved
+      redirect_to restaurant_path(@restaurant)
+    else # if it doesn't save, show the new form again.
+      render 'new.html.erb'
+    end
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'edit.html.erb'
+    end
+  end
+
+  def destory
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
